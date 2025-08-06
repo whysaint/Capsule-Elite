d4d7c1f536cb334f096c89cd1f3656fc36c4d886
WhySaint <taras.panchuk.un@gmail.com>
2025-08-06 22:16:00 +0300

    fix ReferenceNullException

--- Assets/Scripts/LootHeal.cs
+++ Assets/Scripts/LootHeal.cs
@@ -6,7 +6,10 @@ public class LootHeal : MonoBehaviour
     public int HealthValue = 1;
     private void OnTriggerEnter(Collider other)
     {
-        other.attachedRigidbody.GetComponent<PlayerHealth>().AddHealht(HealthValue);
-        Destroy(gameObject);
+        if (other.attachedRigidbody.GetComponent<PlayerHealth>())
+        {
+            other.attachedRigidbody.GetComponent<PlayerHealth>().AddHealht(HealthValue);
+            Destroy(gameObject);
+        }
     }
 }
